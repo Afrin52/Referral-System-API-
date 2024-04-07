567ecf456e455976e64bddacae07b120e1388f88
Afrin <afrinshdih52@gmail.com>
2024-04-07 15:47:26 +0530

    remove

--- referral_system/users/urls.py
+++
@@ -1,8 +0,0 @@
-# from django.urls import path
-# from .views import user_registration, user_details, referrals
-
-# urlpatterns = [
-#     path('register/', user_registration, name='user-registration'),
-#     path('details/', user_details, name='user-details'),
-#     path('referrals/', referrals, name='referrals'),
-# ]
